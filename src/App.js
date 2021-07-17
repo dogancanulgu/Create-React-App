c2d91e411513d845ce6956263f7febcaa35be90f
dogancanulgu <78671556+dogancanulgu@users.noreply.github.com>
2021-07-18 01:35:06 +0300

    Değişken Render Etmek

--- src/App.js
+++ src/App.js
@@ -2,26 +2,38 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
+const name = "Doğancan";
+const surname = "Ülgü"
+
+
 function App() {
 
-  // return React.createElement("div", null, "Hello");
+  return <>
+    <h1>{name} {surname}</h1>
+    <h1>{`Benim Adım ${name}, soyadım ${surname}`}</h1>
+  </>;
 
+
+
+
+  // // return React.createElement("div", null, "Hello");
+
+  // // return (
+  // // <div>
+  // //   <Header />
+  // // </div>);
   // return (
-  // <div>
-  //   <Header />
-  // </div>);
-  return (
-  <>
-    <Header/>
-    <p className="xyz">
-      lorem
-    </p>
-    <label htmlFor="myInput">
-      Name
-    <input id="myInput"/>    
-    </label>
-  </>
-  );
+  // <>
+  //   <Header/>
+  //   <p className="xyz">
+  //     lorem
+  //   </p>
+  //   <label htmlFor="myInput">
+  //     Name
+  //   <input id="myInput"/>    
+  //   </label>
+  // </>
+  // );
 }
 
 export default App;
