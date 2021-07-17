bd119350c545e25c506bc8e77a887cb356157223
dogancanulgu <78671556+dogancanulgu@users.noreply.github.com>
2021-07-18 01:30:14 +0300

    JSX ve temel kurallar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
 function App() {
+
+  // return React.createElement("div", null, "Hello");
+
+  // return (
+  // <div>
+  //   <Header />
+  // </div>);
   return (
-  <div>
-    <Header />
-    <Header />
-    <Header />
-  </div>);
+  <>
+    <Header/>
+    <p className="xyz">
+      lorem
+    </p>
+    <label htmlFor="myInput">
+      Name
+    <input id="myInput"/>    
+    </label>
+  </>
+  );
 }
 
 export default App;
